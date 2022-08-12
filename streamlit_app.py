95e4fb91796834049bd4e34ed68e4067e0b428e2
José Eliel Camargo Molina <j.camargo-molina@imperial.ac.uk>
2022-08-12 23:25:19 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,16 +11,6 @@ import matplotlib.pyplot as plt
 
 fle = Path('posts.csv')
 fle.touch(exist_ok=True)
-"""
-# Welcome to Streamlit!
-
-Edit `/streamlit_app.py` to customize this app to your heart's desire :heart:
-
-If you have any questions, checkout our [documentation](https://docs.streamlit.io) and [community
-forums](https://discuss.streamlit.io).
-
-In the meantime, below is an example of what you can do with just a few lines of code:
-"""
 with open('posts.csv', newline='') as f:
     reader = csv.reader(f)
     posts = list(reader)
